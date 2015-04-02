bb90991f9712bcb0c68190408e051d3ac44921d1
Dragan Milic <dragan@netice9.com>
2015-04-02 20:24:26 +0100

    initial import

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('dockerConsoleApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
